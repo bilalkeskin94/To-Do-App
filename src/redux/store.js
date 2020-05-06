e716e791da8de3ef405bbdd291cbc687ba6db19e
bilalkeskin <bilal@wizio.com.tr>
2020-05-06 09:25:47 +0300

    init

---
+++ src/redux/store.js
@@ -0,0 +1,14 @@
+import { todoReducer, notificationsReducer } from "./reducers";
+import { combineReducers, createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+
+const reducers = combineReducers({
+  todoReducer: todoReducer,
+  notificationsReducer: notificationsReducer,
+});
+//state
+//state.todoReducer //array
+//state.notificationsReducer //obhect
+//state.notificationsReducer.notificationText
+
+export const store = createStore(reducers, applyMiddleware(thunk));
